288a4fa6c708d4ec8bb0db8693928dc1b062f1ec
John Pfuntner <jpfuntne@cisco.com>
2019-02-01 08:08:20 -0500

    Adding scripts from another $HOME/python/

---
+++ hashes.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+import math
+import random
+import string
+
+def random_string():
+  ret = ''
+  while len(ret) < random.randint(0,40):
+    ret += random.choice(string.printable)
+  return ret
+
+def show(val):
+  hashcode = hash(val)
+  # print hashcode
+  exp = int(math.log(math.fabs(hashcode), 2))
+  bits = int(exp)
+  if bits != exp:
+    bits += 1
+  print '{hashcode:32} {bits:6} {val!r}'.format(**locals())
+
+for iteration in range(100):
+  show(random_string())
+  show(random.randint(-2**31, 2**31-1))
---
+++ multikey-yaml.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+import yaml
+import json
+
+txt = """snmp:
+  nms-server:
+    0.0.0.0:
+      community: public
+    0.0.0.0:
+      user:
+        name: test
+        auth-protocol: md0
+        auth-key: admin0
+        priv-protocol: cbc-des
+        priv-key: admin0
+  agent:
+    community: public"""
+
+data = yaml.load(txt)
+print json.dumps(data, indent=2, sort_keys=True)
