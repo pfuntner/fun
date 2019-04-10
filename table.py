b371d9673a026b60144111d3f5dd2a7e633de8be
John Pfuntner <jpfuntne@cisco.com>
2019-04-10 13:59:10 -0400

    Revamping __str__() method
    Originally I was using the Unix column utility to format the columns which worked but it does not allow right-justifying numeric columns.
    
    Also, I wasn't crazy about the idea of using subprocess.Popen() just to format the output.

--- table.py
+++ table.py
@@ -3,8 +3,6 @@
 import logging
 import argparse
 
-import subprocess
-
 class Table(list):
   class Row(object):
     def __init__(self, *args):
@@ -15,16 +13,23 @@ class Table(list):
 
     if 'log' not in globals():
       log = logging.getLogger()
-      
+
     super(Table, self).__init__()
     self.headings = [self.Row(*headings)] if headings else None
 
   def append(self, *cols):
-    super(Table, self).append(self.Row(*cols))
+    super(Table, self).append(self.Row(*(str(col) for col in cols)))
     log.debug('There are now {rows} rows'.format(rows=len(self)))
 
   @staticmethod
   def sorter(col):
+    """
+    Returns a method to be used by sort methods to extract the a comparison key.  
+    I thought this could be used in general if the caller wanted to sort a table 
+    on the fly (`sorted(Table)`) which does not affect the source table but I
+    don't think that's the case.  You can certainly call `sorted(Table)` but
+    it returns a list which is not as useful as having a Table.
+    """
     return lambda row: row.cols[col]
 
   def sort(self, col):
@@ -33,26 +38,72 @@ class Table(list):
 
   def __str__(self):
     log.debug('Rendering string with {rows} rows'.format(rows=len(self)))
-    p = subprocess.Popen('column -t --separator |'.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    for row in (self.headings if self.headings else []) + self:
-      p.stdin.write('|'.join(row.cols) + '\n')
-    p.stdin.close()
-    ret = p.stdout.read().strip('\n')
-    p.wait()
-    return ret
+    return self.format((self.headings if self.headings else []) + self)
+
+  def format(self, rows):
+    # figure width and `numeracy` of columns
+    widths = []
+    isNumerics = []
+    for (row_num, row) in enumerate(rows):
+      for (col_num, col) in enumerate(row.cols):
+        if ((not self.headings) or (row_num > 0)) and (len(self) > 0):
+          try:
+            num = float(col)
+            isNumeric = True
+          except:
+            isNumeric = False
+        else:
+          """
+          if we're processing the heading row, we'll pretend the column is numeric since it shouldn't decide contribute
+          to the decision of whether to left or right-justify the column
+          """
+          isNumeric = True
+
+        if col_num >= len(widths):
+          widths.append(len(col))
+          isNumerics.append(isNumeric)
+        else:
+          widths[col_num] = max(widths[col_num], len(col))
+          isNumerics[col_num] = isNumerics[col_num] and isNumeric
+
+    log.debug('widths: {widths}, isNumerics: {isNumerics}'.format(**locals()))
+
+    ret = []
+    format_string = ' '.join(['{{:{direction}{width}}}'.format(
+      direction='>' if isNumerics[col_num] else '<',
+      width=widths[col_num],
+    ) for col_num in range(len(widths))])
+
+    for (row_num, row) in enumerate(rows):
+      log.debug('Rendering row {row_num}: {row.cols}'.format(**locals()))
+      if self.headings and row_num == 0:
+        ret.append(format_string.replace('{>', '{<').format(*row.cols))
+      else:
+        ret.append(format_string.format(*row.cols))
+
+    return '\n'.join(ret)
 
 if __name__ == '__main__':
+  import math
+
   parser = argparse.ArgumentParser(description='Driver for Table class')
   parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Enable debugging')
+  parser.add_argument('-H', '--headings', dest='headings', action='store_true', help='Use headings')
   parser.add_argument('-l', '--logging', dest='logging', action='store_false', help='Disable logging', default=True)
   args = parser.parse_args()
-  
+
   if args.logging:
     logging.basicConfig(format='%(asctime)s %(levelname)s %(pathname)s:%(lineno)d %(msg)s')
     log = logging.getLogger()
     log.setLevel(logging.DEBUG if args.verbose else logging.WARNING)
 
-  table = Table()
-  table.append('a', 'b', 'c')
-  table.append('This', 'is', 'a test')
+  if args.headings:
+    table = Table('one', 'two', 'three', 'four')
+  else:
+    table = Table()
+  table.append('a', 'b', 'c', '+1')
+  table.append('This', 'is', 'a test', '1.2')
+  table.append('1', '2', '3', 4)
+  table.append('', '', '', 1e50)
+  table.append('easy', 'as', 'pi', math.pi)
   print str(table)
