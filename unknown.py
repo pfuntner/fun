7dc80d5739bd65b74eb778f2382bac76247ca4b6
John Pfuntner <jpfuntne@cisco.com>
2019-08-26 08:13:28 -0400

    Commiting a script to mess with arbitrary options

---
+++ unknown.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Enable debugging')
+(known, unknown) = parser.parse_known_args()
+
+print 'known: {}'.format(known)
+print 'unknown: {}'.format(unknown)
+
+# args = parser.parse_args()
+# 
+# logging.basicConfig(format='%(asctime)s %(levelname)s %(pathname)s:%(lineno)d %(msg)s')
+# log = logging.getLogger()
+# log.setLevel(logging.DEBUG if args.verbose else logging.WARNING)
