4a3bd90b24efbed22770f3d5a6304a7fb3ccd917
John Pfuntner <jpfuntne@cisco.com>
2021-02-23 15:10:28 -0500

    Tweaks

--- mock/command.py
+++ mock/command.py
@@ -1,6 +1,8 @@
+import os
 import subprocess
 
 def exec():
+  print(os.getpid())
   p = subprocess.Popen(['true'])
   rc = p.wait()
   return rc
--- mock/test_command.py
+++ mock/test_command.py
@@ -1,16 +1,22 @@
 import unittest
 import command
 
-class mocked_subprocess(object):
+class mocked_Popen(object):
   def __init__(self, *args):
     pass
 
   def wait(self):
     return 42
 
+def mocked_getpid():
+  return -42
+
+@unittest.mock.patch('command.os')
 @unittest.mock.patch('command.subprocess')
-def test_command(mock_command_subprocess):
-  mock_command_subprocess.Popen = mocked_subprocess
+def test_command(mock_command_subprocess, mock_command_os):
+  mock_command_subprocess.Popen = mocked_Popen
+  mock_command_os.getpid = mocked_getpid
+
   result = command.exec()
   print(f'result = {result}')
   assert result == 42
